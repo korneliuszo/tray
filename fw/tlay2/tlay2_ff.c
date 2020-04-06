75330b04e9650cfb5f6debe99e4bd4777ac1915d
Korneliusz Osmenda <korneliuszo@gmail.com>
2020-04-06 22:48:10 +0200

    Be sure nobody overwrites us, and send reply.

--- fw/tlay2/tlay2_ff.c
+++ fw/tlay2/tlay2_ff.c
@@ -3,6 +3,7 @@
 
 #include <avr/interrupt.h>
 #include <avr/wdt.h>
+#include <util/delay.h>
 
 static volatile uint16_t *const bootKeyPtr = (volatile uint16_t *)0x0800;
 
@@ -30,7 +31,8 @@ uint8_t tlay2_process_ff()
 	case 0x02: //enter bootloader
 		tlay2_tx_init_reply();
 		tlay2_tx_end();
-		//cli();
+		_delay_ms(100);
+		cli();
 		*bootKeyPtr=0x7777;
 		wdt_enable(WDTO_250MS);
 		for (;;);
@@ -38,7 +40,8 @@ uint8_t tlay2_process_ff()
 	case 0x03: //enter app
 		tlay2_tx_init_reply();
 		tlay2_tx_end();
-		//cli();
+		_delay_ms(100);
+		cli();
 		*bootKeyPtr=0xffff;
 		wdt_enable(WDTO_250MS);
 		for (;;);
