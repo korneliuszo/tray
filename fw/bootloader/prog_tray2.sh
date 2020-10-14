8612b18fc3083c71fbc85e6e1a46cbfaedff74e6
Korneliusz Osmenda <korneliuszo@gmail.com>
2020-10-14 18:05:47 +0200

    Add delay so it flashes stock uc

--- fw/bootloader/prog_tray2.sh
+++ fw/bootloader/prog_tray2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#avrdude -c usbasp -p m328p -U lfuse:w:0xe0:m 
-#avrdude -c usbasp -p m328p -U hfuse:w:0xD0:m
-#avrdude -c usbasp -p m328p -U efuse:w:0xff:m
+#avrdude -c usbasp -p m328p -U lfuse:w:0xe0:m -B 1000
+#avrdude -c usbasp -p m328p -U hfuse:w:0xD0:m -B 1000
+#avrdude -c usbasp -p m328p -U efuse:w:0xff:m -B 1000
 avrdude -c usbasp -p m328p -U flash:w:tray2/bootloader.elf 
